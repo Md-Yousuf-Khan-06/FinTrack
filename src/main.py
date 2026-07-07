504c0a7f20b90a991f4485a3bc3eefa6c90250ee
Md-Yousuf-Khan-06 <yk772187@gmail.com>
2026-07-07 18:31:54 +0530

    Add transaction search and expense managemen

--- src/main.py
+++ src/main.py
@@ -4,13 +4,25 @@ payment_methods= {
             3: "Bank Transfer",
             4: "Card"
         }
+
+
 def select_payment_method():
     print("Select payment method:\n 1. Cash\n 2. UPI\n 3. Bank Transfer\n 4. Card")
     payment_method_choice = int(input("Enter your payment method choice:\n"))
     user_payment_method=payment_methods[payment_method_choice]
     return user_payment_method
+
+def display_expense(expense):
+            print(f"Amount: {expense['amount']}")
+            print(f"Category: {expense['category']}")
+            print(f"description: {expense['description']}")
+            print(f"Payment Method: {expense['payment_method']}")
+            print("-------------------------")
+
 income_records=[]
 expense_records=[]
+
+
 choice=0
 while choice!=6:
     print("\nFINTRACK")
@@ -22,6 +34,7 @@ while choice!=6:
     print("5. Monthly Summary")
     print("6. Exit\n")
     choice=int(input("Enter Your Choice: "))
+
     if choice==1:
         amount = int(input("Enter income amount: "))
         source = input("Enter income source: ")
@@ -32,9 +45,9 @@ while choice!=6:
                 "payment_method":payment
             }
         income_records.append(income)
-        # print(income_records)
         print("\n✅ Payment added successfully!\n")
-        # print("Income feature is under development.")
+
+
     elif choice==2:
         expense_amount=int(input('Enter the expense amount:'))
         category=input("Enter the expense category:")
@@ -48,24 +61,67 @@ while choice!=6:
         }
         expense_records.append(expense)
         print("Expense added successfully")
+
+
     elif choice==3:
-        # print("Viewing transactions...")
         print("======TRANSACTIONS======")
+
         print("----INCOME----")
         for income in income_records:
             print(f"Amount: {income['amount']}")
             print(f"Source: {income['source']}")
             print(f"Payment Method: {income['payment_method']}")
             print("-------------------------")
+
         print("----EXPENSE----")
         for expense in expense_records:
-            print(f"Amount: {expense['amount']}")
-            print(f"Category: {expense['category']}")
-            print(f"description: {expense['description']}")
-            print(f"Payment Method: {expense['payment_method']}")
-            print("-------------------------")
+            display_expense(expense)
+        
+
+
     elif choice==4:
-        print("Searching transactions...")
+        print("Search by:\n 1. Category\n 2. Description\n 3. Payment Method\n 4. Back to Menu")
+        user_choice=int(input("Enter Your Choice: "))
+
+        if user_choice==1:
+            category= input("Enter Category: ")
+            found=False
+            for expense in expense_records:
+               if category.lower() in expense["category"].lower():
+                   found=True
+                   display_expense(expense)
+            if not found:
+               print("No matching transactions found")
+
+        elif user_choice==2:
+            description =input("Enter Description: ")
+            found=False
+            for expense in expense_records:
+                if description.lower() in expense["description"].lower():
+                    found=True
+                    display_expense(expense)
+            if not found:
+                print("No matching transactions found.")
+
+        elif user_choice==3:
+            payment_method=select_payment_method()
+            found=False
+            for expense in expense_records:
+                if payment_method in expense["payment_method"]:
+                    display_expense(expense)
+                    found=True
+            if not found:
+                print("No matching transactions found.")
+            
+            # input("Enter Payment Method: ")
+
+        # print("Searching transactions...")
+
+
+
+
+
+
     elif choice==5:
         print("Generating monthly summary...") 
     elif choice==6:
